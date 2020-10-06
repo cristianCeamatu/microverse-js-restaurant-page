a4661da27185fa58ed9793078e1368cd9c9338ca
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-10-06 20:57:30 +0300

    Add Bootstrap Basic Html structure Refactor to module components Favicon Refactor components to receive parameters

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './assets/style.scss';
 
-import _ from 'lodash';
+import Header from './components/Header';
+import Main from './components/Main';
 
 function component() {
   const element = document.createElement('div');
+  const articles = ['Cheap prices', 'Michelin Stars', 'Takeaway'];
+  const header = {
+    title: 'Welcome the Microverse Restaurant',
+    subtitle: 'A place where you will always want to come back',
+  };
 
-  element.innerHTML = _.join(['Hello', 'webpack34'], ' ');
-  element.classList.add('hello');
+  element.appendChild(Header(header));
+  element.appendChild(Main(articles));
 
   return element;
 }
 
-document.body.appendChild(component());
+document.querySelector('#content').appendChild(component());
